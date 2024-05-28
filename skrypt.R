449ec28937bea382cb21c1a1f14941880cb3435e
MateuszMisz <126468490+MateuszMisz@users.noreply.github.com>
2024-05-28 15:52:49 +0200

    data frame z srednimi dla kazdej z kolumn z podzialme na grupy

--- skrypt.R
+++ skrypt.R
@@ -6,14 +6,32 @@
 #if (!file.exists(file_path)) {
  # stop("Podany plik nie istnieje.")
 #}
-file_path
-data<-read.csv(file=file_path)
+file_path<-"C:\\Users\\mysza\\Downloads\\przykladoweDane-Projekt (1).csv"
+data<-read.csv2(file=file_path)
 numeric_columns<-sapply(data,is.numeric)
 #numeric_data<-data[,c(TRUE,numeric_columns[-1])]
+numeric_column_names<-colnames(data[,numeric_columns])
+#numeric_data<-data[,which(data)]
 
-numeric_data<-data[,which(data)]
-for(row in rownames(data)){
-  for (column in colnames(data)){
-    if (column)
-  }
+
+#obliczenie wszystkich srednich
+columnnames<-c()
+for(column in colnames(data)){
+  if(is.numeric(data[,column]))
+    columnnames<-c(columnnames,column)
 }
+groups<-unique(data[,1])
+all_means<-data.frame(
+  group=groups
+)
+for(column in columnnames){
+  mean_vector<-c()
+  for(group in groups){
+    meanv<-mean(data[which(data[,1]==group),column],na.rm=TRUE)
+    mean_vector<-c(mean_vector,meanv)
+  }
+  tmpdf<-data.frame(mean_vector)
+  all_means<-cbind(all_means,tmpdf)
+  names(all_means)[ncol(all_means)]<-column
+}
+rownames(all_means)<-groups
